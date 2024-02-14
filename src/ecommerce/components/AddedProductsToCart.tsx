63fe56e699dff33e60ca8fb61ee43c2479b1d8c9
Adrián Fonseca Vega <afonsecav88@gmail.com>
2024-02-14 00:54:07 -0300

    show product in cart card

--- src/ecommerce/components/AddedProductsToCart.tsx
+++ src/ecommerce/components/AddedProductsToCart.tsx
@@ -7,50 +7,49 @@ import {
   ListItemText,
   Typography,
 } from '@mui/material';
-import React from 'react';
+import { useSelectorAndDispatch } from '../hooks/useSelectorAndDispatch';
 
 export const AddedProductsToCart = () => {
+  const { cartsProducts } = useSelectorAndDispatch();
+  const { cantProdInCart, cartProducts } = cartsProducts;
+
   return (
     <Container
       sx={{
         display: 'flex',
         justifyContent: 'flex-end',
+        alignItems: 'flex-end',
+        alignContent: 'center',
+        textJustify: 'center',
+        flexDirection: 'column',
         margin: '0px',
         minWidth: '100%',
         position: 'sticky',
         top: '185px',
       }}
     >
-      <List
-        sx={{
-          display: 'flex',
-          width: '100%',
-          maxWidth: 360,
-          bgcolor: 'background.paper',
-        }}
-      >
-        <ListItem>
-          <ListItemAvatar>
-            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-          </ListItemAvatar>
-          <ListItemText
-            primary="Brunch this weekend?"
-            secondary={
-              <React.Fragment>
-                <Typography
-                  sx={{ display: 'inline' }}
-                  component="span"
-                  variant="body2"
-                  color="text.primary"
-                >
-                  Ali Connors
-                </Typography>
-                {" — I'll be in your neighborhood doing errands this…"}
-              </React.Fragment>
-            }
-          />
-        </ListItem>
-      </List>
+      {cartProducts &&
+        cartProducts.map((product) => (
+          <List key={product.id}>
+            <ListItem
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <Typography variant="subtitle2" gutterBottom fontWeight="bold">
+                {product.title}
+              </Typography>
+              <Typography variant="subtitle2" gutterBottom>
+                Precio: $ {product.price}
+              </Typography>
+              <Typography variant="subtitle2" gutterBottom>
+                Cantidad: {product.price}
+              </Typography>
+            </ListItem>
+          </List>
+        ))}
     </Container>
   );
 };
