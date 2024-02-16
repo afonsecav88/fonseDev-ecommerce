574d6f6802ca23d765bcc19bc348c40fbb968fd2
Adrián Fonseca Vega <afonsecav88@gmail.com>
2024-02-15 23:30:17 -0300

    created not product in the card

---
+++ src/ecommerce/components/NotProductInCart.tsx
@@ -0,0 +1,58 @@
+import { Alert, AlertTitle, Button, Container } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
+export const NotProductInCart = () => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Container
+        maxWidth="xs"
+        sx={{
+          display: 'flex',
+          width: '450px',
+          justifyContent: 'center',
+          gap: 2,
+        }}
+      >
+        <Alert
+          severity="info"
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            gap: 2,
+          }}
+        >
+          <AlertTitle>
+            {' '}
+            <strong>Aca se mostraran tus productos </strong>
+          </AlertTitle>
+          <svg
+            style={{ display: 'flex', width: '150px', alignSelf: 'center' }}
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+            />
+          </svg>
+
+          <Button
+            onClick={() => navigate('/')}
+            variant="contained"
+            color="success"
+            size="small"
+            sx={{ display: 'flex', marginTop: '10px', alignSelf: 'center' }}
+          >
+            Descubre productos
+          </Button>
+        </Alert>
+      </Container>
+    </>
+  );
+};
--- src/ecommerce/pages/ProductCartDetails.tsx
+++ src/ecommerce/pages/ProductCartDetails.tsx
@@ -10,91 +10,100 @@ import {
 } from '@mui/material';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { useSelectorAndDispatch } from '../hooks/useSelectorAndDispatch';
+import { NotProductInCart } from '../components/NotProductInCart';
 
 export const ProductCartDetails = () => {
   const { cartsProducts } = useSelectorAndDispatch();
-  const { cartProducts } = cartsProducts;
+  const { cartProducts, cantProdInCart } = cartsProducts;
   return (
     <Container sx={{ display: 'flex', minWidth: '1000px' }}>
-      <Card
-        sx={{
-          width: '67%',
-          // position: 'fixed',
-          // top: '80px',
-          // right: '1px',
-          // zIndex: '1500',
-          backgroundColor: '#E2E2E2',
-        }}
-      >
-        <Typography
+      {cantProdInCart ? (
+        <Card
           sx={{
-            display: 'flex',
-            justifyContent: 'space-evenly',
-            gap: 2,
-            padding: 1,
-            backgroundColor: '#51D27F',
-            flexWrap: 'wrap',
+            width: '67%',
+            // position: 'fixed',
+            // top: '80px',
+            // right: '1px',
+            // zIndex: '1500',
+            backgroundColor: '#E2E2E2',
           }}
-          gutterBottom
-          variant="overline"
-          component="div"
         >
-          <strong>Productos en el carrito </strong>
-          <Badge sx={{ marginTop: '10px' }} badgeContent={4} color="primary">
-            <ShoppingCartIcon color="action" />
-          </Badge>
-        </Typography>
-        {cartProducts.map((product) => (
-          <div key={product.id}>
-            <CardContent sx={{ display: 'flex', gap: 1, flexWrap: 'wrap' }}>
-              <Typography
-                sx={{ display: 'inline-flex', gap: 1 }}
-                gutterBottom
-                variant="subtitle1"
-                component="div"
-              >
-                <Avatar src={product.image} />
-              </Typography>
-              <Typography sx={{ width: '40%' }}>{product.title}</Typography>
-              <Typography
-                sx={{ width: '15%' }}
-                variant="body2"
-                color="text.secondary"
-              >
-                <strong>Precio: $ </strong> {product.price}
-              </Typography>
-              <Typography
-                sx={{ width: '15%' }}
-                variant="body2"
-                color="text.secondary"
-              >
-                <strong>Cantidad:</strong> {product.count}
-              </Typography>
-              <Button
-                variant="contained"
-                color="info"
-                size="small"
-                sx={{ float: 'right', width: '40px', height: '40px' }}
-                onClick={() => {}}
-                // startIcon={<ArrowForwardIcon />}
-              >
-                +
-              </Button>
-              <Button
-                variant="contained"
-                color="info"
-                size="small"
-                sx={{ float: 'right', width: '40px', height: '40px' }}
-                onClick={() => {}}
-                // startIcon={<ArrowForwardIcon />}
-              >
-                -
-              </Button>
-            </CardContent>
-            <Divider />
-          </div>
-        ))}
-      </Card>
+          <Typography
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-evenly',
+              gap: 2,
+              padding: 1,
+              backgroundColor: '#51D27F',
+              flexWrap: 'wrap',
+            }}
+            gutterBottom
+            variant="overline"
+            component="div"
+          >
+            <strong>Productos en el carrito </strong>
+            <Badge
+              sx={{ marginTop: '10px' }}
+              badgeContent={cantProdInCart}
+              color="primary"
+            >
+              <ShoppingCartIcon color="action" />
+            </Badge>
+          </Typography>
+          {cartProducts.map((product) => (
+            <div key={product.id}>
+              <CardContent sx={{ display: 'flex', gap: 1, flexWrap: 'wrap' }}>
+                <Typography
+                  sx={{ display: 'inline-flex', gap: 1 }}
+                  gutterBottom
+                  variant="subtitle1"
+                  component="div"
+                >
+                  <Avatar src={product.image} />
+                </Typography>
+                <Typography sx={{ width: '40%' }}>{product.title}</Typography>
+                <Typography
+                  sx={{ width: '15%' }}
+                  variant="body2"
+                  color="text.secondary"
+                >
+                  <strong>Precio: $ </strong> {product.price}
+                </Typography>
+                <Typography
+                  sx={{ width: '15%' }}
+                  variant="body2"
+                  color="text.secondary"
+                >
+                  <strong>Cantidad:</strong> {product.count}
+                </Typography>
+                <Button
+                  variant="contained"
+                  color="info"
+                  size="small"
+                  sx={{ float: 'right', width: '40px', height: '40px' }}
+                  onClick={() => {}}
+                  // startIcon={<ArrowForwardIcon />}
+                >
+                  +
+                </Button>
+                <Button
+                  variant="contained"
+                  color="info"
+                  size="small"
+                  sx={{ float: 'right', width: '40px', height: '40px' }}
+                  onClick={() => {}}
+                  // startIcon={<ArrowForwardIcon />}
+                >
+                  -
+                </Button>
+              </CardContent>
+              <Divider />
+            </div>
+          ))}
+        </Card>
+      ) : (
+        <NotProductInCart />
+      )}
     </Container>
   );
 };
