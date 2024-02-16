9acc7e89b59f16dc361cfef1a1871c230c1163da
Adrián Fonseca Vega <afonsecav88@gmail.com>
2024-02-16 15:40:29 -0300

    fixed header to maked responsive

--- src/ecommerce/shared/components/Header.tsx
+++ src/ecommerce/shared/components/Header.tsx
@@ -20,89 +20,91 @@ export const Header = () => {
           },
           padding: 1,
           cursor: 'pointer',
-          marginBottom: 5,
+          marginBottom: '20px',
           minWidth: '100%',
+          alignItems: 'center',
+          height: '120px',
         }}
       >
-        <Container maxWidth="xl">
-          <Container
+        <Container
+          maxWidth="xl"
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            gap: { xs: 0, md: 1, xl: 3 },
+            flexWrap: 'wrap',
+            marginRight: 10,
+            height: '50px',
+          }}
+        >
+          <Typography variant="h1" sx={{ flexGrow: 1 }}>
+            <Link to={'/ecommerce'}>
+              <Logo />
+            </Link>
+          </Typography>
+          <Button
+            color="inherit"
+            component={NavLink}
+            to="electronics"
             sx={{
-              display: 'flex',
-              flexDirection: { xs: 'column', md: 'row' },
-              justifyContent: 'center',
-              gap: { xs: 0, md: 4 },
-              flexWrap: 'wrap',
-              marginRight: 10,
+              mt: 1,
+              color: '#fff',
+              '&:hover': {
+                color: 'rgb(226, 194, 145)',
+              },
             }}
           >
-            <Typography variant="h1" sx={{ flexGrow: 1 }}>
-              <Link to={'/ecommerce'}>
-                <Logo />
-              </Link>
-            </Typography>
-            <Button
-              color="inherit"
-              component={NavLink}
-              to="electronics"
-              sx={{
-                mt: 3.5,
-                color: '#fff',
-                '&:hover': {
-                  color: 'rgb(226, 194, 145)',
-                },
-              }}
-            >
-              electronics
-            </Button>
-            <Button
-              color="inherit"
-              component={NavLink}
-              to="jewelery"
-              sx={{
-                mt: 3.5,
-                color: '#fff',
-                '&:hover': {
-                  color: 'rgb(226, 194, 145)',
-                },
-              }}
-            >
-              jewelery
-            </Button>
-            <Button
-              color="inherit"
-              component={NavLink}
-              to="men-clothing"
-              sx={{
-                mt: 3.5,
-                color: '#fff',
-                '&:hover': {
-                  color: 'rgb(226, 194, 145)',
-                },
-              }}
-            >
-              men's clothing
-            </Button>
-            <Button
-              color="inherit"
-              component={NavLink}
-              to="women-clothing"
-              sx={{
-                mt: 3.5,
-                color: '#fff',
-                '&:hover': {
-                  color: 'rgb(226, 194, 145)',
-                },
-              }}
-            >
-              women's clothing
-            </Button>
-            <ShoppingCart />
-          </Container>
-          <Container sx={{ display: 'flex', justifyContent: 'center', my: 2 }}>
-            <Search />
-          </Container>
+            electronics
+          </Button>
+          <Button
+            color="inherit"
+            component={NavLink}
+            to="jewelery"
+            sx={{
+              mt: 1,
+              color: '#fff',
+              '&:hover': {
+                color: 'rgb(226, 194, 145)',
+              },
+            }}
+          >
+            jewelery
+          </Button>
+          <Button
+            color="inherit"
+            component={NavLink}
+            to="men-clothing"
+            sx={{
+              mt: 1,
+              color: '#fff',
+              '&:hover': {
+                color: 'rgb(226, 194, 145)',
+              },
+            }}
+          >
+            men's clothing
+          </Button>
+          <Button
+            color="inherit"
+            component={NavLink}
+            to="women-clothing"
+            sx={{
+              mt: 1,
+              color: '#fff',
+              '&:hover': {
+                color: 'rgb(226, 194, 145)',
+              },
+            }}
+          >
+            women's clothing
+          </Button>
+          <ShoppingCart />
+        </Container>
+        <Container sx={{ display: 'flex', justifyContent: 'center', my: 1 }}>
+          <Search />
         </Container>
       </AppBar>
+
       <Box sx={{ display: { xs: 'block', sm: 'block', md: 'none' } }}>
         <AppDrawer />
       </Box>
