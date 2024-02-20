e77446416e274634c15978b431acbf2a05acbc08
Adrián Fonseca Vega <afonsecav88@gmail.com>
2024-02-19 23:06:11 -0300

    create card drawer

--- src/ecommerce/components/CartDrawer.tsx
+++ src/ecommerce/components/CartDrawer.tsx
@@ -1,9 +1,24 @@
-import { Container, Button, Drawer } from '@mui/material';
+import {
+  Container,
+  Button,
+  Drawer,
+  Box,
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from '@mui/material';
 import { useState } from 'react';
 import { useSelectorAndDispatch } from '../hooks/useSelectorAndDispatch';
-import { ProductInCartCard } from './ProductInCartCard';
+import { NavLink } from 'react-router-dom';
+import { Logo } from '../shared/components/Logo';
+import { ShoppingCart } from './ShoppingCart';
+import { useUpdateProductInCart } from '../hooks/useUpdateProductInCart';
 
 export default function CartDrawer() {
+  const { navigate } = useUpdateProductInCart();
   const { cartsProducts } = useSelectorAndDispatch();
   const { cartProducts } = cartsProducts;
   const [open, setOpen] = useState(false);
@@ -15,7 +30,39 @@ export default function CartDrawer() {
       </Button>
 
       <Drawer anchor="right" open={open} onClose={() => setOpen(false)}>
-        <ProductInCartCard cartProducts={cartProducts} />
+        {/* <ProductInCartCard cartProducts={cartProducts} /> */}
+
+        <Box sx={{ auto: 250 }} role="presentation">
+          <NavLink to={'/ecommerce'}>
+            <Logo />
+          </NavLink>
+          <List>
+            {cartProducts.map(({ id, count, image, price, title }) => (
+              <ListItem key={id} disablePadding>
+                <ListItemButton>
+                  {/* <NavLink
+                    className={({ isActive }) =>
+                      isActive ? 'nav-active' : 'disable-visited-color'
+                    }
+                    style={{ textDecoration: 'none' }}
+                    to={path.toLocaleLowerCase()}
+                  > */}
+                  <ListItemIcon>{image}</ListItemIcon>
+                  <ListItemText primary={title} />
+                  {/* </NavLink> */}
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            <ListItemButton onClick={() => navigate(`cart/details`)}>
+              <ListItem sx={{ display: 'flex' }}>
+                <ShoppingCart />
+              </ListItem>
+            </ListItemButton>
+          </List>
+        </Box>
       </Drawer>
     </Container>
   );
